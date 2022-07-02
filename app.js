1ec3fa409ced1c576414bd8e5ded78f99dc6b65e
Fegi Sucepto Priawan <fegisucepto6997@gmail.com>
2022-07-03 00:35:31 +0700

    initial commit

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-// if (process.env.NODE_ENV !== "production") {
-//   require("dotenv").config();
-// }
-require("dotenv").config();
-const cors = require("cors");
-const express = require("express");
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+require('dotenv').config();
+const cors = require('cors');
+const express = require('express');
 const app = express();
 // const port = 3000;
 const port = process.env.PORT || 3000;
-const router = require("./router/index.js");
+const router = require('./router/index.js');
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use("/", router);
+app.use('/', router);
 
 app.listen(port, () => {
   console.log(`This program is running`, port);
